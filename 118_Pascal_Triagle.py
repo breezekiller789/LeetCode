346e82a929b98626ff44142b9ac89c7ff14429c5
Willy Huang <breezekiller789@csie.io>
2021-03-16 23:11:44 +0800

    排列組合中的排列而且，階乘可以互消來加速，剩下只是迴圈控制

---
+++ 118_Pascal_Triagle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# https://leetcode.com/problems/pascals-triangle/
+
+# 其實就只是排列組合當中的排列，也就是C，C 10取3那個C，階乘可以消掉，可以加速很
+# 多，剩下的就只是迴圈去控制，離散中的二項式定理(1+x)^n
+
+
+def Combination(n, k):
+    molecular = 1       # 分子
+    denominator = 1     # 分母
+    # C 10取8等於 C 10取2，但為了要快，越小越好。
+    if k > n/2:
+        k = n - k
+    # 算分子
+    for i in xrange(n, n-k, -1):
+        molecular *= i
+    # 算分母
+    for i in range(1, k+1):
+        denominator *= i
+    return molecular / denominator
+
+
+n = 4
+numRows = 5
+Ans = []
+for i in range(numRows):
+    Cur_Level = []
+    for j in range(i+1):
+        Cur_Level.append(Combination(i, j))
+    Ans.append(Cur_Level)
+print Ans
