c6148789b97fe0c9b5dc0740cc9a6e570d2d8cd3
breezekiller789 <breezekiller789@csie.io>
2021-05-01 22:52:55 +0800

    I'm really close, but still unsolved

--- 428_Serialize_And_Deserialize_N_ary_Tree.py
+++ 428_Serialize_And_Deserialize_N_ary_Tree.py
@@ -12,7 +12,7 @@ class Node(object):
 
 root = Node(1, [])
 root.children.extend([Node(3, []), Node(2, []), Node(4, [])])
-root.children[0].children.extend([Node(5, []), Node(6, [])])
+root.children[1].children.extend([Node(5, []), Node(6, [])])
 #                1
 #         3      2       4
 #      5     6
@@ -22,16 +22,49 @@ class Codec(object):
     # Level order traversal
     def serialize(self, root):
         Q = [root]
+        Result = []
+        String = ""
         while Q:
-            currentNode = Q[0]
-            print currentNode.val
-            Q = Q[1:]
-            for child in currentNode.children:
-                Q.append(child)
+            nodesOfCurrentLevel = len(Q)
+            Result.append([])
+            while nodesOfCurrentLevel > 0:
+                currentNode = Q[0]
+                if currentNode == "null":
+                    String += "null,"
+                    Q = Q[1:]
+                    nodesOfCurrentLevel -= 1
+                    continue
+                Q = Q[1:]
+                String += "{},".format(currentNode.val)
+                Result[-1].append(currentNode.val)
+                for child in currentNode.children:
+                    Q.append(child)
+                if not currentNode.children:
+                    Q.append("null")
+                nodesOfCurrentLevel -= 1
+            String += "null,"
+        print String[:-1].split(",")
+        return String[:-1].split(",")
 
     def deserialize(self, data):
-        pass
+        root = Node(data[0], [])
+        data = data[2:]
+        Q = [root]
+        while Q:
+            nodesOfCurrentLevel = len(Q)
+            while nodesOfCurrentLevel > 0:
+                currentNode = Q[0]
+                Q = Q[1:]
+                while data[0] != "null":
+                    newNode = Node(data[0], [])
+                    currentNode.children.append(newNode)
+                    Q.append(newNode)
+                    data = data[1:]
+                data = data[1:]
+                nodesOfCurrentLevel -= 1
+        return root
 
 
 obj = Codec()
-obj.serialize(root)
+newroot = obj.deserialize(obj.serialize(root))
+obj.serialize(newroot)
