5c8a3fcf47504a87f7e18e3be36c02ced85c5c9c
breezekiller789 <breezekiller789@csie.io>
2021-05-07 11:16:23 +0800

    LCS with recursion, DP

--- Longest_Common_Subsequence.py
+++ Longest_Common_Subsequence.py
@@ -15,4 +15,23 @@ def LCS(s1, s2):
         return max(LCS(s1[1:], s2), LCS(s1, s2[1:]))
 
 
-print LCS("appl", "applet")
+def LCS_DP(s1, s2):
+    lens1 = len(s1)
+    lens2 = len(s2)
+    dp = [[None] * (lens2+1) for i in range(lens1+1)]
+    i = 0
+    while i < lens1+1:
+        j = 0
+        while j < lens2+1:
+            if i == 0 or j == 0:
+                dp[i][j] = 0
+            elif s1[i-1] == s2[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            j += 1
+        i += 1
+    return dp[lens1][lens2]
+
+
+print LCS_DP("applet", "appl")
