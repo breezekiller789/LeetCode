cf0e326e64c0ab2f6ee4ab06a2e1f1c110d7c2f4
breezekiller789 <breezekiller789@csie.io>
2021-05-10 13:46:00 +0800

    Hash tables + sorting

--- 594_Longest_Harmonious_Subsequence.py
+++ 594_Longest_Harmonious_Subsequence.py
@@ -7,4 +7,17 @@ nums = [1, 3, 2, 2, 5, 2, 3, 7]
 # nums = [1, 2, 3, 4]
 # nums = [1, 1, 1, 1]
 
-Diffs = [0 for _ in range(len(nums))]
+hashTable = dict()
+for num in nums:
+    if num not in hashTable:
+        hashTable[num] = 1
+    else:
+        hashTable[num] += 1
+myList = hashTable.items()
+myList.sort(key=lambda x: x[0])
+Sum = 0
+Max = 0
+for idx, num in enumerate(myList[:-1]):
+    if abs(num[0]-myList[idx+1][0]) == 1:
+        Sum = max(Sum, num[1]+myList[idx+1][1])
+print Sum
