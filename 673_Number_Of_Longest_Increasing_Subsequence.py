db928a3658b7af1b42d2bd1e73c4c2245923af75
breezekiller789 <breezekiller789@csie.io>
2021-05-13 19:05:39 +0800

    Unsolved but i'm pretty close, i can get longest increasing subsequnce now

--- 673_Number_Of_Longest_Increasing_Subsequence.py
+++ 673_Number_Of_Longest_Increasing_Subsequence.py
@@ -3,6 +3,9 @@
 
 # https://leetcode.com/problems/number-of-longest-increasing-subsequence/
 
+# 我很接近了，現在我可以回傳nums中最長的遞增subsequence，而且不用是連續的！
+# 我覺得這樣就是一個DP考題了，因為我就是用DP下去解，但是跟題目要的東西不一樣。
+
 nums = [1, 3, 5, 4, 7]
 # nums = [2, 2, 2, 2, 2]
 
