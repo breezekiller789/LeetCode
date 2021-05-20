517064c574d1c13d1d849431c2536cc1b129634b
breezekiller789 <breezekiller789@csie.io>
2021-05-20 13:11:54 +0800

    回來重新解一次，解開了，就是做一些邊界檢查

--- 19_Remove_Nth_Node_From_End_Of_List.py
+++ 19_Remove_Nth_Node_From_End_Of_List.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 
+# 先走一次list，把每一個節點都放進一個list，最重要就是要做一些邊界檢查。
+
 
 class ListNode(object):
     def __init__(self, val=0, next=None):
@@ -9,67 +11,35 @@ class ListNode(object):
         self.next = next
 
 
-def List_Insert(head, num):
-    tmp = head
-    while tmp.next:
-        tmp = tmp.next
-    # print "Insert {}".format(num)
-    tmp.next = ListNode(num)
+class Solution(object):
+    def __init__(self):
+        self.haha = 0
+
+    def removeNthFromEnd(self, head, n):
+        self.haha = None
+        tmp = head
+        allNodes = []
+        while tmp:
+            allNodes.append(tmp)
+            tmp = tmp.next
+        if len(allNodes) == 1:
+            return None
+        if len(allNodes) == n:
+            return allNodes[0].next
+        allNodes[-(n+1)].next = allNodes[-n].next
+        return allNodes[0]
 
 
-head = ListNode()
-try:
-    while 1:
-        num = input()
-        List_Insert(head, num)
-except EOFError:
-    pass
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+n = 2
 
-# tmp = head.next
-# while tmp:
-#     print tmp.val
-#     tmp = tmp.next
-
-n = 1
-Previous = []
-
-# 如果n == 1代表就是把尾巴去掉
-if n == 1:
-    if not head.next.next:
-        print []
-        exit()
-    tmp = head
-    while tmp.next.next:
-        tmp = tmp.next
-    del tmp.next
-    exit()
-
-head = head.next            # 略過dummy head
-
-i, tmp = 0, head
-while i <= n and tmp:
-    Previous.append(tmp)
-    tmp = tmp.next
-    i += 1
-if not tmp:
-    print head.next.val
-    exit()
-
-# for i in range(n+1):        # 存n+1個節點，因為要刪除第n個，必須要記錄上一個
-#     Previous.append(tmp)
-#     tmp = tmp.next
-
-# 走到這裡Previous就裝滿了n+1個節點了，可以開始從頭往下
-tmp = head
-while tmp:
-    Previous.append(tmp)        # 當遇到新的就新增上去
-    del Previous[0]             # 然後把最前面的刪掉
-    tmp = tmp.next
-
-Previous[0].next = Previous[1].next     # 這兩步就是在做刪除的動作
-del Previous[1]
-
-tmp = head
+obj = Solution()
+result = obj.removeNthFromEnd(head, n)
+tmp = result
 while tmp:
     print tmp.val
     tmp = tmp.next
