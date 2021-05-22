7ec28d8cf460c4e5c2338817c9bbaa20e5237172
breezekiller789 <breezekiller789@csie.io>
2021-05-22 19:27:39 +0800

    Too hard...

---
+++ 135_Candy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# https://leetcode.com/problems/candy/
+
+ratings = [1, 0, 2]   # 5, [2, 1, 2]
+# ratings = [1, 2, 2]   # 4, [1, 2, 1]
+
+candy = [1 for _ in range(len(ratings))]
