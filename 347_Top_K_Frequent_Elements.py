99fb494c32d5f943a011e03e309c4b638969784e
breezekiller789 <breezekiller789@csie.io>
2021-05-28 23:30:05 +0800

    5k likes medium, Hashing + Sorting

--- 347_Top_K_Frequent_Elements.py
+++ 347_Top_K_Frequent_Elements.py
@@ -4,10 +4,32 @@
 
 nums = [1, 1, 1, 2, 2, 3]
 k = 2
-Sets = {}
-for i in nums:
-    if str(i) not in Sets:
-        Sets[str(i)] = 1
-    else:
-        Sets[str(i)] += 1
-print Sets
+
+
+class Solution(object):
+    def __init__(self):
+        self.haha = 0
+
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        self.haha = 0
+        hashTable = dict()
+        for num in nums:
+            if num not in hashTable:
+                hashTable[num] = 1
+            else:
+                hashTable[num] += 1
+        extractedList = hashTable.items()
+        extractedList.sort(key=lambda x: x[1], reverse=1)
+        result = []
+        for element in extractedList[:k]:
+            result.append(element[0])
+        return result
+
+
+obj = Solution()
+print obj.topKFrequent(nums, k)
