fde410c16f5f87e492f3db7b6bb9d9c89820946f
breezekiller789 <breezekiller789@csie.io>
2021-06-03 21:31:42 +0800

    3.6k likes hard, DP problem, Unsolved

--- 312_Burst_Balloons.py
+++ 312_Burst_Balloons.py
@@ -11,25 +11,48 @@
 # =========Code===========
 
 
-def Pick_Smallest(Nums):
-    Min = (float("inf"), 0)
-    for idx, i in enumerate(Nums):
-        if i < Min[0]:
-            Min = i, idx+1
-    return Min
-
-
+# Output: 167
+# Explanation:
+# nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+# coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
 nums = [3, 1, 5, 8]
-nums = [1, 5]
-nums = [9, 76, 64, 21, 97, 60]
-Sum = 0
-while nums[1:-1]:
-    smallestNum, idx = Pick_Smallest(nums[1:-1])
-    Sum += nums[idx-1]*nums[idx]*nums[idx+1]
-    del nums[idx]
-    # print nums, smallestNum
-if nums[0] > nums[-1]:
-    Sum += nums[0]*nums[-1]+nums[0]
-else:
-    Sum += nums[-1]*nums[0]+nums[-1]
-print Sum
+
+# Output: 10
+# nums = [1, 5]
+
+# nums = [9, 76, 64, 21, 97, 60]
+
+nums = [8, 2, 6, 8, 9, 8, 1, 4, 1, 5, 3, 0, 7, 7, 0, 4, 2, 2, 5, 5]
+
+
+# 我覺得我快解出來了，但是遇到上面這個測資會TLE
+def RecursiveBurst(nums, alreadyChecked):
+    length = len(nums)
+    if length == 1:
+        return nums[0]
+    if tuple(nums) in alreadyChecked:
+        return alreadyChecked[tuple(nums)]
+    maxPoint = float("-inf")
+    # 從第一個氣球開始搓，然後看搓哪一個氣球分數最高
+    for i in range(length):
+        if tuple(nums[:i]+nums[i+1:]) in alreadyChecked:
+            ret = alreadyChecked[tuple(nums[:i]+nums[i+1:])]
+        else:
+            ret = RecursiveBurst(nums[:i]+nums[i+1:], alreadyChecked)
+        left = 1
+        mid = nums[i]
+        right = 1
+        if i >= 1:
+            left = nums[i-1]
+        if i+1 < length:
+            right = nums[i+1]
+        currentPoints = left * mid * right
+        maxPoint = max(maxPoint, ret+currentPoints)
+        alreadyChecked[tuple(nums[:i]+nums[i+1:])] = ret
+    alreadyChecked[tuple(nums)] = maxPoint
+    return maxPoint
+
+
+alreadyChecked = dict()
+print RecursiveBurst(nums, alreadyChecked)
+# print alreadyChecked
