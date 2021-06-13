57c26dced2fc43be2281d37fabc3678c2658119b
breezekiller789 <breezekiller789@csie.io>
2021-06-14 00:34:31 +0800

    Unsolved

--- 870_Advantage_Shuffle.py
+++ 870_Advantage_Shuffle.py
@@ -3,50 +3,44 @@
 
 # https://leetcode.com/problems/advantage-shuffle/
 
-# 有bug
+# 1. Sort nums1
+# 2. Use the snail crawling method
+
 
 # Output: [2,11,7,15]
 nums1 = [2, 7, 11, 15]
 nums2 = [1, 10, 4, 11]
 
 # Output: [24,32,8,12]
+# [8, 12, 24, 32]
 # nums1 = [12, 24, 8, 32]
 # nums2 = [13, 25, 32, 11]
 
-# Sort nums1
-# loop through nums2 one by one
-# apply binary search
-
-
-def Find(nums, target):
-    if nums[0] > target or target > max(nums):
-        ret = nums[0]
-        del nums[0]
-        return ret
-
-    index = 1
-    while index < len(nums) and nums[index] < target:
-        index *= 2
-
-    if index >= len(nums):
-        ret = nums[-1]
-        del nums[-1]
-        return ret
-    if nums[index] == target:
-        ret = nums[index]
-        del nums[index]
-        return ret
-
-    while nums[index] > target:
-        index -= 1
-    ret = nums[index+1]
-    del nums[index+1]
-    return ret
-
-
 nums1.sort()
-result = []
-for num in nums2:
-    res = Find(nums1, num)
-    result.append(res)
+length = len(nums1)
+taken = [False for _ in range(length)]
+result = [None for _ in range(length)]
+if nums2[0] < nums1[0]:
+    result[0] = nums1[0]
+    taken[0] = True
+for i, num in enumerate(nums2):
+    snailPosition = 1
+    while snailPosition < length and num >= nums1[snailPosition]:
+        snailPosition *= 2
+    if snailPosition >= length:
+        snailPosition /= 2
+        while snailPosition < length and num >= nums1[snailPosition]:
+            snailPosition += 1
+        if snailPosition >= length:
+            for idx, istaken in enumerate(taken):
+                if not istaken:
+                    result[i] = nums1[idx]
+                    taken[idx] = True
+                    break
+        else:
+            result[i] = nums1[snailPosition]
+            taken[snailPosition] = True
+    elif num < nums1[snailPosition] and not result[i]:
+        result[i] = nums1[snailPosition]
+        taken[snailPosition] = True
 print result
